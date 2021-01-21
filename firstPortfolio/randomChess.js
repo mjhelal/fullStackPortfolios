4a3062c9029c9b0eb643fd0b33e084f45c3381ec
mjhelal@gmail.com <mjhelal@gmail.com>
2021-01-21 16:22:03 -0300

    first commit

---
+++ firstPortfolio/randomChess.js
@@ -0,0 +1,3 @@
+let chessPlayers = ['Alekhine','Capablanca','Lasker','Morphy','Kasparov','Fisher','Carlsen']
+numPlayer = Math.floor(7*Math.random())
+console.log(`The best Chess player in history is: ${chessPlayers[numPlayer]}`)
